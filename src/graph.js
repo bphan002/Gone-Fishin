d005564f92899e4c0c4fadefa64ab638b645aab5
Billy <billy.phan4@gmail.com>
2022-04-27 16:11:16 -0700

    refactor code

--- src/graph.js
+++ src/graph.js
@@ -1,17 +1,13 @@
-import {select, scaleBand, scaleLinear, axisLeft, axisTop, axisBottom, scaleExtent } from "d3"
+import {select, scaleBand, scaleLinear, axisLeft, axisBottom } from "d3"
 
 const svg = select('svg')
-const width = +svg.attr('width') //from index.html
-const height = +svg.attr('height') // from index.html
-let fishAbundancy = {}
-let graphTitle;
-let xAxisLabel;
+const width = +svg.attr('width')
+const height = +svg.attr('height')
 
+export function graph(data) {
+    svg.selectAll("*").remove()
 
-
-export function render(data) {
-    svg.selectAll("*").remove(); // removes previous graph
-    var margin = {
+    const margin = {
         top: 15,
         right: 50,
         bottom: 250,
@@ -20,82 +16,71 @@ export function render(data) {
     const graphWidth = width - margin.left - margin.right
     const graphHeight = height - margin.top - margin.bottom
 
-
-
-    // console.log(data)
-    data = data.slice(0, 20)
+    data = data.slice(0, 20) //to make graph still readable
+    
+    const root = svg.append('g') // group element
+        .attr('transform', `translate(${margin.left},${margin.top})`)
+    
     const xScale = scaleBand()
-        // console.log(data.Protein)
-        // .domain([0,max(data, fish => fish.Sodium)])
         .domain(data.map(fish => fish.name))
         .range([0, graphWidth])
         .padding(0.1)
-    // console.log(xScale.domain())
-
 
     const yScale = scaleLinear()
         .domain([0, 250])
-        .range([graphHeight, margin.bottom + margin.top])  //width of bars for bar chart
-    
-    const g = svg.append('g') // group element
-        .attr('transform', `translate(${margin.left},${margin.top})`)
+        .range([graphHeight, margin.bottom + margin.top]) 
 
-        
-    g.selectAll('rect').data(data)
-        .enter().append('rect')
-        .attr('x', fish => xScale(fish.name))
-        .attr('y', fish => yScale(fish.calories))
-        .attr('height', fish => graphHeight - yScale(fish.calories))
-        .style("fill", fish => fish.color)
-        .transition().duration(2000)
-        .delay((d, i) => i * .5)
-        .attr('width', xScale.bandwidth())
-     
-        
+    root.selectAll('rect').data(data)
+        .enter()
+        .append('rect')
+            .attr('x', fish => xScale(fish.name))
+            .attr('y', fish => yScale(fish.calories))
+            .attr('height', fish => graphHeight - yScale(fish.calories))
+            .style("fill", fish => fish.color)
+            .transition().duration(2000)
+            .delay((d, i) => i * .5)
+            .attr('width', xScale.bandwidth())
 
-    const xAxisG= g.append("g")
-        .attr("transform", `translate(0,${graphHeight})`)
-        .call(axisBottom(xScale))
-        
-
+    //dynamic way to make graph more readable
     let rotation = -90
     let anchor = "end"
     if (data.length < 5) {
         rotation = 0
         anchor = "middle"
     }
-    g.selectAll("text")
-        .attr("transform", `translate(-12,10)rotate(${rotation})`)
+
+    root.selectAll("text")
+        .attr("transform", `translate(-12,10) rotate(${rotation})`)
         .attr("text-anchor", anchor )
         .style("fill", "black")
         .style("font-size", 36 - data.length)
 
     const yAxis = axisLeft(yScale) //putting axes label
-    yAxis(g.append('g'))  // adds the left axis label
+    yAxis(root.append('g'))  // adds the left axis label
 
     svg.append("text")
-    .attr("class", "y label")
-    .attr("text-anchor", "end")
-    .attr("y", margin.left - 50)
-    .attr("x", -graphHeight/2)
-    .style("font-size", "35px")
-    // .attr('transform', "translate('600÷','600')")
-    .attr("transform", "rotate(-90)")
-    .text("Calories");
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("y", margin.left - 50)
+        .attr("x", -graphHeight/2)
+        .style("font-size", "35px")
+        .attr("transform", "rotate(-90)")
+        .text("Calories");
     
-    data.length > 1 ? xAxisLabel = "Fish Names" : xAxisLabel = "Fish Name"
-    svg.append("text")
-    .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("y", graphHeight + margin.bottom)
-    .attr("x", margin.right + (width/2))
-    .style("font-size", "35px")
-    // .attr('transform', "translate('600÷','600')")
-    // .attr("transform", "rotate(180)")
-    .text(xAxisLabel);
+   
+    const xAxisLabel = data.length > 1 ? "Fish Names" : "Fish Name"
 
-    data.length > 1 ? graphTitle = "Fishes" : graphTitle = "Fish"
-    g.append("text")
+    svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("y", graphHeight + margin.bottom)
+        .attr("x", margin.right + (width/2))
+        .style("font-size", "35px")
+        .text(xAxisLabel);
+
+    const graphTitle = data.length > 1 ? "Fishes" : "Fish"
+    
+    root.append("text")
         .attr("x",(graphWidth/2))
         .attr("y", (20+ margin.top + margin.bottom))
         .attr("text-anchor", "middle")
--- src/index.js
+++ src/index.js
@@ -1,47 +1,40 @@
-import {render} from "./graph"
+import { graph } from "./graph"
 
-const fishProfileContainer = document.querySelector("[fish-profile-container]")
-const fishProfileTemplate = document.querySelector("[fish-profile-template]")
-const search = document.querySelector("[data-search]")
+const fishProfileContainer = document.getElementById("fish-profile-container")
 
-let fishGraph = []
-let fishes; 
+let fishToGraph = []
+let allFish = []
 
-document.addEventListener("DOMContentLoaded",()=> start())
+document.addEventListener("DOMContentLoaded", () => start())
 
 async function start() {
-    addLinks()
+    configLinks()
 
     fishProfileContainer.addEventListener("click", (e) => {
         const fish = e.target.parentNode
         const id = fish.dataset['id']
-        singleFishPage(fishes[id]) //grabs whole fishes array
+        updateFishProfile(allFish[id])
     });
 
-
-    //this is for searchbar
-    search.addEventListener("input", e => {
+    const searchInput = document.getElementById("search")
+    searchInput.addEventListener("input", e => {
         const value = e.target.value.toLowerCase()
-        // console.log(fishes)
-        searchResult(fishes,value)
+        showFishMatches(allFish, value)
     })
- 
-    async function fetchfishes() {
+
+    async function fetchFishes() {
         let restResponse = await fetch("https://www.fishwatch.gov/api/species")
         let data = await restResponse.json()
-    
-        return data.map(createFish)  //map will use createFish to make an object with fish parameters from api data
-        //data is an array of object with fish properties
+        return data.map(createFish)
     }
 
-    fishes = await fetchfishes() //same as above
-    fishes = fishes.filter(fish => fish.image) //don't need this anymore
-    fishes.forEach(renderFish) //render fish with ids and these are the icons we set ask about children
-    render(fishes) // for graph
+    allFish = await fetchFishes()
+    allFish.forEach(addFishIcon)
+    graph(allFish) //sample graph
 }
 
 
-function singleFishPage(fish) {
+function updateFishProfile(fish) {
     const container = document.getElementById("single-fish")
     const name = document.getElementById("fish-name")
     const descriptionTitle = document.getElementById("fish-description-title")
@@ -50,29 +43,31 @@ function singleFishPage(fish) {
     const location = document.getElementById('fish-location')
     const locationTitle = document.getElementById("fish-location-title")
     const button = container.querySelector('button')
-    
+
     name.innerHTML = fish.name
     locationTitle.innerHTML = fish.location ? "Location" : ""
     location.innerHTML = fish.location
     descriptionTitle.innerHTML = fish.description ? "Description" : ""
     description.innerHTML = fish.description
     image.src = fish.image
-    applyButtontext(fish,button)
+    applyButtontext(fish, button)
 
-    button.onclick = () => {   
+    button.onclick = () => {
         fish.selected = !fish.selected
         if (fish.selected) {
-            fishGraph.push(fish)
+            fishToGraph.push(fish)
         } else {
-            fishGraph = fishGraph.filter((ele) => {
-                return ele !== fish 
+            fishToGraph = fishToGraph.filter((ele) => {
+                return ele !== fish
             })
         }
-        applyButtontext(fish,button)
-        render(fishGraph)
+        applyButtontext(fish, button)
+        graph(fishToGraph)
     }
 
-    function applyButtontext(fish,button) {
+    changePage("single-fish")
+
+    function applyButtontext(fish, button) {
         if (fish.selected) {
             button.innerHTML = "Remove Fish from Graph"
             button.classList.add("remove-graph")
@@ -80,14 +75,13 @@ function singleFishPage(fish) {
             button.innerHTML = "Add Fish to Graph"
             button.classList.remove("remove-graph")
         }
-        
-    }
 
-    changePage("single-fish")
+    }
 }
 
-function renderFish(fish,index) { //index is used for the html so we can figure out what fish it is when we click on it
-    const profile = fishProfileTemplate.content.cloneNode(true).children[0] //difference between this and append?
+function addFishIcon(fish, index) {
+    const fishProfileTemplate = document.getElementById("fish-profile-template")
+    const profile = fishProfileTemplate.content.cloneNode(true).children[0]
     const name = profile.querySelector("h3")
     const image = profile.querySelector("img")
     image.src = fish.image
@@ -96,55 +90,54 @@ function renderFish(fish,index) { //index is used for the html so we can figure 
     profile.dataset.id = index
 }
 
-//will take fish object
-//will work with a single copy versus our other render fish function
 
 function createFish(fish) {
-    let ret = {} 
-    ret.image = fish["Species Illustration Photo"].src 
-    ret.name = fish["Species Name"]
-    ret.description = fish["Physical Description"]
-    ret.location = fish["Location"]
-    ret.selected = false
-    ret.calories = fish['Calories']
+    let fishObj = {}
+    fishObj.image = fish["Species Illustration Photo"].src
+    fishObj.name = fish["Species Name"]
+    fishObj.description = fish["Physical Description"]
+    fishObj.location = fish["Location"]
+    fishObj.selected = false
+    fishObj.calories = fish['Calories']
 
     let populationSentence = fish.Population
     // console.log(populationSentence)
     if (populationSentence === null) {
-        ret.status = "unknown"
-        ret.color = "black"
+        fishObj.status = "unknown"
+        fishObj.color = "black"
     } else if (populationSentence.toLowerCase().includes("significantly below")) {
-        ret.status = "significantly below"
-        ret.color = "red"
+        fishObj.status = "significantly below"
+        fishObj.color = "red"
     } else if (populationSentence.toLowerCase().includes("significantly above")) {
-        ret.status = "significantly above"
-        ret.color = "green"
+        fishObj.status = "significantly above"
+        fishObj.color = "green"
     } else if (populationSentence.toLowerCase().includes("above")) {
-        ret.status = 'above'
-        ret.color = "lightgreen"
+        fishObj.status = 'above'
+        fishObj.color = "lightgreen"
     } else if (populationSentence.toLowerCase().includes("below")) {
-        ret.status = "below"
-        ret.color = "orange"
+        fishObj.status = "below"
+        fishObj.color = "orange"
     } else if (populationSentence.toLowerCase().includes("unknown")) {
-        ret.status = "unknown"
-        ret.color = "black"
+        fishObj.status = "unknown"
+        fishObj.color = "black"
     } else if (populationSentence.toLowerCase().includes("near target level")) {
-        ret.status = "near target level"
-        ret.color = "pink"
+        fishObj.status = "near target level"
+        fishObj.color = "pink"
     } else {
         console.log(populationSentence)
     }
 
-    return ret
+    return fishObj
 }
 
-function searchResult(fishes,value){
-    const fishEle= document.querySelectorAll("[data-id]")
+function showFishMatches(fishes, value) {
+    const fishEle = document.querySelectorAll("[data-id]")
     fishEle.forEach(ele => {
         let index = ele.dataset.id
-        const isVisible =fishes[index].name.toLowerCase().includes(value)
+        const isVisible = fishes[index].name.toLowerCase().includes(value)
         ele.classList.toggle("hide", !isVisible)
     })
+
 }
 
 //find all of the nav items that have data attribute data-page
@@ -152,31 +145,45 @@ function searchResult(fishes,value){
 //add event listenrs to each of those
 // loop through all of the elements prev stored and hide them unless data matches
 
-function getPageConfig(){
+function getPageConfig() {
     return {
         links: document.querySelectorAll("header [data-page]"),
         pages: document.querySelectorAll("main [data-page]")
     }
 }
 
-function addLinks() {
+function configLinks() {
     const config = getPageConfig()
     config.links.forEach(link => {
-        link.addEventListener('click',()=>{
-            changePage(link.dataset.page)
+        link.addEventListener('click', () => {
+            changePage(link.dataset.page) //sting of the page id
         })
     })
-} 
+}
 
-function changePage(targetPage){
+function changePage(targetPage) {
     const config = getPageConfig()
     config.pages.forEach(page => {
         if (page.dataset.page === targetPage) {
-                page.classList.remove('invisible')
-                page.classList.add('visible')
+            page.classList.remove('invisible')
+            page.classList.add('visible')
         } else {
-                page.classList.add('invisible')
-                page.classList.remove('visible')
+            page.classList.add('invisible')
+            page.classList.remove('visible')
         }
     })
 }
+
+
+///if i put an event listner on single-fish
+
+//it would be triggered on any children of single-fish
+
+//target that will give you is the acdtual target elemnt that you click on
+
+//parent elmeent is jsut the parent elemnt of that item
+
+//if you have different level of nesting liek this
+//it can be one of 3 things
+
+//
